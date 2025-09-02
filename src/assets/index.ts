b9561dbf06a7f714827426a8656854003789bfd9
Tsega Getnet Yehuala <tsega.get.grace@gmail.com>
2025-09-03 00:41:20 +0300

    add icons

---
+++ src/assets/index.ts
@@ -0,0 +1,66 @@
+// Import company logos
+import mathwork from "./company/mathwork.png";
+import edunet from "./company/edunet.png";
+import eduskill from "./company/eduskill.png";
+
+// Import tech stack images
+import python from "./tech-stack/python.png";
+import java from "./tech-stack/java.png";
+
+// Import service icons
+import fullstackIcon from "./services/fullstack.png";
+import backendIcon from "./services/backend.svg";
+import frontendIcon from "./services/frontend.svg";
+import databaseIcon from "./services/database.svg";
+import devopsIcon from "./services/devops.svg";
+
+// Import tech logos
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import vuejs from "./tech/vue.png";
+import docker from "./tech/docker.png";
+import django from "./tech/django.png";
+import sql from "./tech/sql.png";
+import mongo from "./tech/mongo.png";
+import ts from "./tech/ts.png";
+
+// Import common assets
+import logo from "./logo.svg";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+export {
+  fullstackIcon,
+  backendIcon,
+  frontendIcon,
+  databaseIcon,
+  devopsIcon,
+  mathwork,
+  edunet,
+  python,
+  java,
+  github,
+  css,
+  git,
+  html,
+  javascript,
+  nodejs,
+  reactjs,
+  tailwind,
+  vuejs,
+  logo,
+  menu,
+  close,
+  eduskill,
+  docker,
+  django,
+  sql,
+  mongo,
+  ts,
+};
