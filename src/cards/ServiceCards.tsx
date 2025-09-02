0dfad829e3e74a4f892c0aa213ab0a09c6abb130
Tsega Getnet Yehuala <tsega.get.grace@gmail.com>
2025-09-02 08:46:57 +0300

    @tailwindcss/upgrade

--- src/cards/ServiceCards.tsx
+++ src/cards/ServiceCards.tsx
@@ -14,7 +14,7 @@ export const ServiceCard = ({ index, title, icon }) => (
     <div className="xs:w-[250px] w-full">
       <motion.div
         variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+        className="w-full green-pink-gradient p-px rounded-[20px] shadow-card"
       >
         <div className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
           <img
